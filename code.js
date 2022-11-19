05857efdee21f303c49b1cd75e53761508f73678
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2022-11-19 20:38:17 +0200

    even better async functions

--- code.js
+++ code.js
@@ -1,20 +1,20 @@
 
 let func = async () => {
-    let response = await (await fetch('https://accept.paymob.com/api/auth/tokens', {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        
-        "api_key": "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2libUZ0WlNJNkltbHVhWFJwWVd3aUxDSndjbTltYVd4bFgzQnJJam8yTWpZNE5qbDkuRUNhck5jVk4xNlV2c0tYZ2JzdUFRRFdLX1dCUlBPU2dpLUh3U0J0TUM5WmUwd1Yzem10YW1ydmVaQU9xci1LZ1hYUkZWUTA1eVhDdkRTMllzY3Y3Znc="
-    
-    })
-})).json();
+    let response = await fetch('https://accept.paymob.com/api/auth/tokens', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
 
+            "api_key": "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2libUZ0WlNJNkltbHVhWFJwWVd3aUxDSndjbTltYVd4bFgzQnJJam8yTWpZNE5qbDkuRUNhck5jVk4xNlV2c0tYZ2JzdUFRRFdLX1dCUlBPU2dpLUh3U0J0TUM5WmUwd1Yzem10YW1ydmVaQU9xci1LZ1hYUkZWUTA1eVhDdkRTMllzY3Y3Znc="
+
+        })
+    }).then(resp => resp.json());
     let token = response.token;
-    document.getElementById("token").innerHTML=token;
+    console.log(token)
+    document.getElementById("token").innerHTML = token;
 }
 
 func();
