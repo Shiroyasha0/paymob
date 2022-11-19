a46ef5c3df9ce05f97d3e844e6b0635ef8523a4f
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2022-11-19 20:37:07 +0200

    using async functions

--- code.js
+++ code.js
@@ -1,7 +1,6 @@
 
-var x;
-
-let y = fetch('https://accept.paymob.com/api/auth/tokens', {
+let func = async () => {
+    let response = await (await fetch('https://accept.paymob.com/api/auth/tokens', {
     method: 'POST',
     headers: {
         'Accept': 'application/json',
@@ -12,12 +11,10 @@ let y = fetch('https://accept.paymob.com/api/auth/tokens', {
         "api_key": "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2libUZ0WlNJNkltbHVhWFJwWVd3aUxDSndjbTltYVd4bFgzQnJJam8yTWpZNE5qbDkuRUNhck5jVk4xNlV2c0tYZ2JzdUFRRFdLX1dCUlBPU2dpLUh3U0J0TUM5WmUwd1Yzem10YW1ydmVaQU9xci1LZ1hYUkZWUTA1eVhDdkRTMllzY3Y3Znc="
     
     })
-})
-.then(response => response.json())
-.then(response => JSON.stringify(response.token))
-.then(token => {
-    document.getElementById("token").innerHTML=token;
-    return token;
-})
+})).json();
 
-y.then((token)=> console.log(token));
+    let token = response.token;
+    document.getElementById("token").innerHTML=token;
+}
+
+func();
