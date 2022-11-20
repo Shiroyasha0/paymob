4c99d0bb6bb59967dc8b5361507451f3ca2afcb2
shiroyasha0 <islamomar5444>
2022-11-20 19:47:56 +0200

    timeout add

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
-randoNum = Math.floor(Math.random()*1000000)
+randoNum = Math.floor(Math.random()*1000000);
+setTimeout(250);
 console.log(randoNum)
 
 
@@ -65,4 +66,23 @@ let get_id = async () => {
 
 get_id();
 
+
+
+let token_func = async () => {
+    let {token} = await fetch('https://accept.paymob.com/api/auth/tokens', {
+        method: 'POST',                                                                
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+
+            "api_key": "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2libUZ0WlNJNkltbHVhWFJwWVd3aUxDSndjbTltYVd4bFgzQnJJam8yTWpZNE5qbDkuRUNhck5jVk4xNlV2c0tYZ2JzdUFRRFdLX1dCUlBPU2dpLUh3U0J0TUM5WmUwd1Yzem10YW1ydmVaQU9xci1LZ1hYUkZWUTA1eVhDdkRTMllzY3Y3Znc="
+        })
+    }).then(resp => resp.json());
+
+   
+    console.log(token)
+    return token;
+}
 
